31c14dbd5ae9c98cedfbda92cd44a8236c47494e
adritadeb <adritad2019@gmail.com>
2022-04-17 01:18:24 +0600

    header done and route setup

---
+++ src/Pages/Shared/Header/Header.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { Container, Nav, Navbar } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import './Header.css';
+
+const Header = () => {
+    return (
+        <div>
+            <Navbar className='header-nav' collapseOnSelect expand="lg">
+                <Container>
+                    <Navbar.Brand as={Link} to="/">Wedding-Portrait</Navbar.Brand>
+                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                    <Navbar.Collapse id="responsive-navbar-nav">
+                        <Nav className="me-auto">
+                            <Nav.Link href="home#Services">Services</Nav.Link>
+                            <Nav.Link href="home#pricing">Pricing</Nav.Link>
+                            <Nav.Link as={Link} to="/blogs">Blogs</Nav.Link>
+                        </Nav>
+                        <Nav>
+                            <Nav.Link as={Link} to="/login">Login</Nav.Link>
+                            <Nav.Link as={Link} eventKey={2} to="/registration">
+                                Registration
+                            </Nav.Link>
+                            <Nav.Link as={Link} to="/about">About me</Nav.Link>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+        </div>
+    );
+};
+
+export default Header;
