502e26f83e3589736417feeedb2b36c3f0f057ad
adritadeb <adritad2019@gmail.com>
2022-04-17 20:45:39 +0600

    sign out applied

--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -1,9 +1,18 @@
+import { signOut } from 'firebase/auth';
 import React from 'react';
 import { Container, Nav, Navbar } from 'react-bootstrap';
+import { useAuthState } from 'react-firebase-hooks/auth';
 import { Link } from 'react-router-dom';
+import auth from '../../../firebase.init';
 import './Header.css';
 
 const Header = () => {
+    const [user] = useAuthState(auth);
+
+    const handleSignOut = () => {
+        signOut(auth);
+    }
+
     return (
         <div>
             <Navbar className='header-nav p-3' collapseOnSelect expand="lg">
@@ -17,11 +26,16 @@ const Header = () => {
                             <Nav.Link as={Link} to="/blogs">Blogs</Nav.Link>
                         </Nav>
                         <Nav>
-                            <Nav.Link as={Link} to="/login">Login</Nav.Link>
+                            <Nav.Link as={Link} to="/about">About me</Nav.Link>
                             <Nav.Link as={Link} eventKey={2} to="/registration">
                                 Registration
                             </Nav.Link>
-                            <Nav.Link as={Link} to="/about">About me</Nav.Link>
+                            {
+                                user ?
+                                    <button onClick={handleSignOut} className='btn btn-link text-decoration-none text-white'>Sign out</button>
+                                    :
+                                    <Nav.Link as={Link} to="/login">Login</Nav.Link>
+                            }
                         </Nav>
                     </Navbar.Collapse>
                 </Container>
