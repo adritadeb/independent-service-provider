731dbda35bf1d791e54bc375728e7ab8878ac214
adritadeb <adritad2019@gmail.com>
2022-04-17 15:39:22 +0600

    login and register form design done

--- src/Pages/Login/Registration/Registration.js
+++ src/Pages/Login/Registration/Registration.js
@@ -1,9 +1,29 @@
 import React from 'react';
+import { Button, Form } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
 const Registration = () => {
     return (
-        <div>
-
+        <div className='w-50 mx-auto my-5'>
+            <h1 className='text-center form-title'>Registration</h1>
+            <Form>
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                    <Form.Label>Name</Form.Label>
+                    <Form.Control type="text" placeholder="Your name" />
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                    <Form.Label>Email address</Form.Label>
+                    <Form.Control type="email" placeholder="Enter email" />
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="formBasicPassword">
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control type="password" placeholder="Password" />
+                </Form.Group>
+                <Button className='w-50 submit-btn' type="submit">
+                    Register
+                </Button>
+            </Form>
+            <p>Already have an account? <Link className='text-decoration-none' to='/login'>Please Login</Link></p>
         </div>
     );
 };
