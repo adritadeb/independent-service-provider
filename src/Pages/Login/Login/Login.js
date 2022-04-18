c756e1d5694fd02fec9af1d7adfcdef5b5799ed9
adritadeb <adritad2019@gmail.com>
2022-04-18 15:54:10 +0600

    login form updated

--- src/Pages/Login/Login/Login.js
+++ src/Pages/Login/Login/Login.js
@@ -1,17 +1,20 @@
 import React, { useRef } from 'react';
 import { Button, Form } from 'react-bootstrap';
-import { useSignInWithEmailAndPassword, useSendPasswordResetEmail } from 'react-firebase-hooks/auth';
+import { useSendPasswordResetEmail, useSignInWithEmailAndPassword } from 'react-firebase-hooks/auth';
 import { Link, useLocation, useNavigate } from 'react-router-dom';
+import { toast, ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import auth from '../../../firebase.init';
 import Loading from '../../Shared/Loading/Loading';
 import SocialLogin from '../SocialLogin/SocialLogin';
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-import './Login.css';
 
 const Login = () => {
     const emailRef = useRef('');
     const passwordRef = useRef('');
+
+    const location = useLocation();
+    let from = location.state?.from?.pathname || "/";
+    const navigate = useNavigate();
     const [
         signInWithEmailAndPassword,
         user,
@@ -19,9 +22,6 @@ const Login = () => {
         error,
     ] = useSignInWithEmailAndPassword(auth);
     const [sendPasswordResetEmail, sending] = useSendPasswordResetEmail(auth);
-    const navigate = useNavigate();
-    const location = useLocation();
-    const from = location?.state?.from?.pathname || '/';
     let errorElement;
 
     const handleLogin = event => {
@@ -32,14 +32,6 @@ const Login = () => {
         signInWithEmailAndPassword(email, password);
     }
 
-    if (loading || sending) {
-        return <Loading></Loading>
-    }
-
-    if (error) {
-        errorElement = <p className='text-danger'>Error: {error?.message}</p>
-    }
-
     const resetPassword = async () => {
         const email = emailRef.current.value;
         if (email) {
@@ -47,8 +39,16 @@ const Login = () => {
             toast('Sent email');
         }
         else {
-            toast('Please enter your email address')
+            toast('Please enter your email');
         }
+    }
+
+    if (loading || sending) {
+        return <Loading></Loading>
+    }
+
+    if (error) {
+        errorElement = <p className='text-danger'>Error: {error.message}</p>
     }
 
     if (user) {
@@ -58,25 +58,27 @@ const Login = () => {
     return (
         <div className='w-50 mx-auto my-5'>
             <h1 className='text-center form-title'>Login</h1>
-            <Form onClick={handleLogin}>
+            <Form onSubmit={handleLogin}>
                 <Form.Group className="mb-3" controlId="formBasicEmail">
                     <Form.Label>Email address</Form.Label>
-                    <Form.Control className='fs-5' ref={emailRef} type="email" placeholder="Enter email" required />
+                    <Form.Control ref={emailRef} type="email" placeholder="Enter email" required />
                 </Form.Group>
+
                 <Form.Group className="mb-3" controlId="formBasicPassword">
                     <Form.Label>Password</Form.Label>
-                    <Form.Control className='fs-5' ref={passwordRef} type="password" placeholder="Password" required />
+                    <Form.Control ref={passwordRef} type="password" placeholder="Password" required />
                 </Form.Group>
+
                 <Button className='w-50 d-block mx-auto text-white border-0 m-3 py-2 submit-btn' type="submit">
                     Login
                 </Button>
             </Form>
             {errorElement}
             <SocialLogin></SocialLogin>
-            <ToastContainer />
             <p>New to Wedding-Portrait? <Link className='text-decoration-none' to='/registration'>Please Register</Link></p>
-            <p>Forgot password? <button onClick={resetPassword} className='btn btn-link text-decoration-none'>Reset password</button></p>
-        </div >
+            <p>Forgot Password? <span onClick={resetPassword} className='btn btn-link text-decoration-none'>Reset Password</span></p>
+            <ToastContainer />
+        </div>
     );
 };
 
